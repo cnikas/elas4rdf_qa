6c83423b298ac6732f4df4f9c4354c4ef9fe0420
chris <chrisniks1@gmail.com>
2020-11-24 23:36:30 +0200

    init code

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ import json
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] =  'e2b35432632f190f45201266'
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+print('Initializing...')
+ae = AnswerExtraction()
+atp = AnswerTypePrediction()
+print('\tDONE')
+
+
 
 @app.route('/answer', methods=['GET'])
 def api_answer():
@@ -84,13 +90,6 @@ def api_more_entities():
     entities = ar.get_entities_updated(selected_entities,selected_type)
 
     return jsonify(entities)
-
-@app.before_first_request
-def _initialize():
-    print('Initializing...')
-    ae = AnswerExtraction()
-    atp = AnswerTypePrediction()
-    print('\tDONE')
    
 if __name__ == "__main__":
     print('Initializing...')
